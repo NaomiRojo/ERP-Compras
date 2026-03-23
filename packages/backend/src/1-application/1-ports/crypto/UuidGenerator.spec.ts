2623c72b1d000dbb857a35dfba47689f63f91c7a
NaomiRojo <andre.rojo.m@mail.com>
2026-03-22 20:41:17 -0400

    Limpieza + docker funcionando + estructura ordenada

--- packages/backend/src/1-application/1-ports/crypto/UuidGenerator.spec.ts
+++
@@ -1,7 +0,0 @@
-import { type UuidGenerator } from "src/1-application/1-ports/crypto/UuidGenerator";
-import { mock } from "bun:test";
-import { faker } from "@faker-js/faker";
-
-export const mockUuidGenerator: () => UuidGenerator = () => ({
-  generate: mock(() => faker.string.uuid())
-});
--- packages/backend/src/1-application/1-ports/env/Config.spec.ts
+++
@@ -1,9 +0,0 @@
-import { faker } from "@faker-js/faker";
-import { type Config } from "src/1-application/1-ports/env/Config";
-
-const fromZeroSecondsToAYear = () => faker.number.int({ min: 0, max: 370 * 24 * 60 * 60 });
-
-export const mockConfig = (): Config => ({
-  jwtValiditySeconds: fromZeroSecondsToAYear(),
-  refreshTokenValiditySeconds: fromZeroSecondsToAYear()
-});
